d0f10f48e2efbce28381221c7124eb06dc1f8991
khizha <khizhnyakanna@gmail.com>
2021-07-05 19:30:39 +0300

    catching JSON decode error in response

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from json.decoder import JSONDecodeError
 import requests
 
-payload = {"name": "User"}
-response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
-#response = requests.get("https://playground.learnqa.ru/api/get_text")
+response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
+
+try:
+    parsed_response_text = response.json()
+    print(parsed_response_text["answer"])
+except JSONDecodeError:
+    print("Response is not a JSON format data")
