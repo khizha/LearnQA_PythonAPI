9ed8b3a426b9b51a299cad8fc88cb6fa1ba9ad0c
khizha <khizhnyakanna@gmail.com>
2021-07-05 19:16:56 +0300

    JSON parsing example from the lecture "Парсинг JSON"

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 
-#response = requests.get("https://playground.learnqa.ru/api/hello")
-response = requests.get("https://playground.learnqa.ru/api/get_text")
+payload = {"name": "User"}
+response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
+#response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
