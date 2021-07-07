7ade0a73970f7e99507d69fcfd59767ee85e16c1
khizha <khizhnyakanna@gmail.com>
2021-07-07 22:11:04 +0300

    login as one user and try to get data for another user

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -11,7 +11,7 @@ class TestUserGet(BaseCase):
         Assertions.assert_json_has_not_key(response, "firstName")
         Assertions.assert_json_has_not_key(response, "lastName")
 
-    def test_get_user_detailes_auth_as_same_user(self):
+    def test_get_user_details_auth_as_same_user(self):
         data = {
             'email': 'vinkotov@example.com',
             'password': '1234'
@@ -29,4 +29,28 @@ class TestUserGet(BaseCase):
         )
 
         expected_fields = ["username", "email", "firstName", "lastName"]
+        Assertions.assert_json_has_keys(response2, expected_fields)
+
+
+    def test_get_user_details_auth_as_different_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response1 = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+        user_id_from_auth_method = self.get_json_value(response1, "user_id")
+
+        different_user_id = "1"
+
+        print(user_id_from_auth_method)
+
+        response2 = MyRequests.get(f"/user/{different_user_id}",
+                                 headers={"x-csrf-token": token},
+                                 cookies={"auth_sid": auth_sid})
+
+        expected_fields = ["username"]
         Assertions.assert_json_has_keys(response2, expected_fields)
