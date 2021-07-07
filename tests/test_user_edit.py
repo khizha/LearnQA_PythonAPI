e53fa8f4bec1b4c9d014d71a5cde10844d79480b
khizha <khizhnyakanna@gmail.com>
2021-07-07 23:13:43 +0300

    edit user data when authorized as another user

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -65,3 +65,52 @@ class TestUserEdit(BaseCase):
 
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"Auth token not supplied", f"Unexpected response content '{response.content}'"
+
+    def test_edit_user_when_authorized_as_another_user(self):
+        # REGISTER one user
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data["email"]
+        password = register_data["password"]
+
+        # REGISTER another user
+        register_data = self.prepare_registration_data()
+        response11 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response11, 200)
+        Assertions.assert_json_has_key(response11, "id")
+
+        user_id_two = self.get_json_value(response11, "id")
+
+        # LOGIN as the first created user
+        login_data = {
+            'email': email,
+            'password': password
+        }
+
+        response2 = MyRequests.post("/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        # EDIT
+        new_name = "Changed Name"
+
+        print(user_id_two)
+
+        response3 = MyRequests.put(
+            f"/user/{user_id_two}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"firstName": new_name}
+        )
+
+        #print(response3.content)
+
+        Assertions.assert_code_status(response3, 200)
+        assert response3.content.decode("utf-8") == f"", f"Unexpected response content '{response3.content}'"
+
