1b9b14b2b63d96e7d5e9465ab69e32b8529b18ee
khizha <khizhnyakanna@gmail.com>
2021-07-07 23:25:35 +0300

    edit current user e-mail: try to enter the one without '@'

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -114,3 +114,41 @@ class TestUserEdit(BaseCase):
         Assertions.assert_code_status(response3, 200)
         assert response3.content.decode("utf-8") == f"", f"Unexpected response content '{response3.content}'"
 
+    def test_edit_email_of_just_created_user(self):
+        # try to change the e-mail value to incorrect one with missing '@' sign
+
+        # REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data["email"]
+        first_name = register_data["firstName"]
+        password = register_data["password"]
+        user_id = self.get_json_value(response1, "id")
+
+        # LOGIN
+        login_data = {
+            'email': email,
+            'password': password
+        }
+
+        response2 = MyRequests.post("/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        # EDIT
+        new_email = 'abracadabraexample.com'
+
+        response3 = MyRequests.put(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"email": new_email}
+        )
+
+        Assertions.assert_code_status(response3, 400)
+        assert response3.content.decode("utf-8") == f"Invalid email format", f"Unexpected response content '{response3.content}'"
