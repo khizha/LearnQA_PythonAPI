517acf9edce1a618061c20dc04d4714ae2c50f32
khizha <khizhnyakanna@gmail.com>
2021-07-08 01:32:38 +0300

    try to delete a user when logged in as another user

--- tests/test_user_delete.py
+++ tests/test_user_delete.py
@@ -65,3 +65,46 @@ class TestUserDelete(BaseCase):
 
         Assertions.assert_code_status(response4, 404)
         assert response4.content.decode("utf-8") == f"User not found", f"Unexpected response content '{response4.content}'"
+
+    def test_delete_user_when_logged_in_as_another_user(self):
+        # register a new user
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data["email"]
+        password = register_data["password"]
+        user_id = self.get_json_value(response1, "id")
+
+        # login as the created user to get cookies and headers
+        response11 = MyRequests.post("/user/login", data=register_data)
+        Assertions.assert_code_status(response11, 200)
+        auth_sid = self.get_cookie(response11, "auth_sid")
+        token = self.get_header(response11, "x-csrf-token")
+
+        # login as user with id =2
+        login_data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response2 = MyRequests.post("/user/login", data=login_data)
+        Assertions.assert_code_status(response2, 200)
+
+        # try to delete the created user
+        data = {
+            'email': email,
+            'password': password
+
+        }
+
+        response3 = MyRequests.delete(f"/user/{user_id}",
+                                    data=data,
+                                    headers={"x-csrf-token": token},
+                                    cookies={"auth_sid": auth_sid})
+
+
+        Assertions.assert_code_status(response3, 200)
+        assert response3.content.decode("utf-8") == f"", f"Unexpected response content '{response3.content}'"
+
