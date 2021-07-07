6516a921076467ff8f2a48e1687225b0810de4da
khizha <khizhnyakanna@gmail.com>
2021-07-08 00:57:15 +0300

    try to get just deleted user's data

--- tests/test_user_delete.py
+++ tests/test_user_delete.py
@@ -24,3 +24,44 @@ class TestUserDelete(BaseCase):
         Assertions.assert_code_status(response2, 400)
         assert response2.content.decode("utf-8") == f"Please, do not delete test users with ID 1, 2, 3, 4 or 5.", f"Unexpected response content '{response2.content}'"
 
+
+    def test_delete_just_created_user(self):
+        # REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data["email"]
+        password = register_data["password"]
+        user_id = self.get_json_value(response1, "id")
+
+        # LOGIN
+        login_data = {
+            'email': email,
+            'password': password
+        }
+
+        response2 = MyRequests.post("/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        # DELETE
+        response3 = MyRequests.delete(f"/user/{user_id}",
+                                    data=login_data,
+                                    headers={"x-csrf-token": token},
+                                    cookies={"auth_sid": auth_sid})
+
+        Assertions.assert_code_status(response3, 200)
+
+        # try to get the just deleted user's data by ID
+        response4 = MyRequests.get(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        Assertions.assert_code_status(response4, 404)
+        assert response4.content.decode("utf-8") == f"User not found", f"Unexpected response content '{response4.content}'"
