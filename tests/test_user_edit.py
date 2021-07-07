7fc51307d95c9befc30374fb6387016e203e72bd
khizha <khizhnyakanna@gmail.com>
2021-07-07 23:57:23 +0300

    try to set firstName attribute of the current user to one-symbol long value

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -1,6 +1,8 @@
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+import random
+import string
 
 class TestUserEdit(BaseCase):
     def test_edit_just_created_user(self):
@@ -152,3 +154,45 @@ class TestUserEdit(BaseCase):
 
         Assertions.assert_code_status(response3, 400)
         assert response3.content.decode("utf-8") == f"Invalid email format", f"Unexpected response content '{response3.content}'"
+
+    def test_edit_firstname_of_just_created_user(self):
+        # try to change the firstName attribute value to one-symbol long one
+
+        # REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data["email"]
+        password = register_data["password"]
+        user_id = self.get_json_value(response1, "id")
+
+        # LOGIN
+        login_data = {
+            'email': email,
+            'password': password
+        }
+
+        response2 = MyRequests.post("/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        # EDIT
+        short_first_name = random.choice(string.ascii_letters)
+
+        #print(short_first_name)
+
+        response3 = MyRequests.put(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"firstName": short_first_name}
+        )
+
+        #print(response3.content)
+
+        Assertions.assert_code_status(response3, 200)
+        assert response3.content.decode("utf-8") == f"", f"Unexpected response content '{response3.content}'"
